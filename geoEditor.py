2d2fd6b0d6fabec051bd3b7c6f49107e05e4b6bd
GeoGalvanic <gqs@case.edu>
2021-07-08 01:02:52 +0200

    First commit

---
+++ geoEditor.py
@@ -0,0 +1,12 @@
+'''
+Main script for initializing GeoEditor functions and UI.
+
+Created by: Geal Q. Sarrett 08JUL21
+'''
+
+import matplotlib
+import geopandas as gpd
+import tkinter as tk
+import logging
+
+
